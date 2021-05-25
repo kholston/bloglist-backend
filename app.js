0a635e835404ebc6468d46cd4b52e92dd140fbc1
Ken Holston <git.kholston@gmail.com>
2021-05-25 16:44:02 -0400

    exercise 4.2

--- app.js
+++ app.js
@@ -1,34 +1,23 @@
-require('dotenv').config()
-const http = require('http')
+const config = require('./utils/config')
 const express = require('express')
-const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const blogRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
 
+const app = express()
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('error connecting to MongoDB:', error)
+  })
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogRouter)
 
 module.exports = app
--- models/blog.js
+++ models/blog.js
@@ -1,10 +1,18 @@
 const mongoose =  require('mongoose')
 
 const blogSchema = new mongoose.Schema({
-  title: {type: String, required: true },
-  author: {type: String, required: true },
+  title: { type: String, required: true },
+  author: { type: String, required: true },
   url: { type: String, required: true },
-  likes: {type: Number}
+  likes: { type: Number }
 })
 
-module.exports = mongoose.model("Blog", blogSchema)
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
