fa1b4dfdfd372f9003bc20bedf98bb3db43ac3d6
Ken Holston <git.kholston@gmail.com>
2021-05-25 11:30:17 -0400

    moved Blog model to own file

--- app.js
+++ app.js
@@ -4,15 +4,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const Blog = require('./models/blog')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
