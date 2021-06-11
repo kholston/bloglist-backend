ded5e2f663e7b5eece9f8cb6a26ae466153ca606
Ken Holston <git.kholston@gmail.com>
2021-06-11 13:27:34 -0400

    exercise 4.17

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,9 @@ const User = require('../models/user')
 
 
 userRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User
+    .find({})
+    .populate('blogs', { url: 1, title: 1, author: 1 })
   response.json(users)
 })
 
@@ -27,7 +29,8 @@ userRouter.post('/', async (request, response) => {
   const user = new User({
     username: body.username,
     name: body.name,
-    passwordHash
+    passwordHash,
+    blogs:[]
   })
 
   const savedUser = await user.save()
--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ const userSchema = new mongoose.Schema({
   username: { type:String, required: true, minlength: 3 },
   name: { type:String, required:true, minlength: 3 },
   passwordHash: { type: String, required: true, minlength: 3 },
+  blogs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Blog' }]
 })
 
 userSchema.set('toJSON', {
